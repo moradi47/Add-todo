21bb6b5fd7663eff3d9329b415f2b7c3216a3c8f
ms.moradi <ms.moradi47@gmail.com>
2024-11-18 15:53:02 +0330

    Writing function on input to add to do to to-do list and error handling when nothing is typed in input

---
+++ Js/main.js
@@ -0,0 +1,36 @@
+let $ = document;
+
+let inputValue = $.querySelector('.getTodo');
+let liElem = $.querySelector('.todo');
+let ulElem = $.querySelector('#List');
+let divElem = $.querySelector('.list');
+let divAddTodo = $.querySelector('.addTodo');
+let pWrong = $.createElement('p');
+
+
+
+inputValue.addEventListener('keydown', function(event){
+    if(event.key === "Enter"){
+        if(inputValue.value){
+
+            pWrong.style.display = 'none';
+            let newLiItem = $.createElement('li');
+            let newSpan = $.createElement('span');
+            newSpan.innerHTML = inputValue.value;
+            let newIElem = $.createElement('i');
+            newIElem.className = 'fa-solid fa-trash';
+            newLiItem.className = 'todo';
+
+            newLiItem.append(newSpan);
+            newLiItem.append(newIElem);
+            ulElem.append(newLiItem);
+            inputValue.value = '';
+
+        }else{
+            pWrong.innerHTML = "Wrong! Please enter your desired job."
+            pWrong.className = 'wrong';
+            pWrong.style.display = 'block';
+            divAddTodo.append(pWrong);
+        }
+    }
+})
